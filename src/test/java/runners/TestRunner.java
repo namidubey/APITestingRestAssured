f9bd18f4fb3d3ba3857c292c3a8a408660b67d36
namita dubey <namita.dubey@appdirect.com>
2020-12-28 18:08:03 +0530

    correcting folder name

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -7,7 +7,7 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features = "src/test/resources/Feature",
+		features = "src/test/resources/features",
 		glue = {"step_definitions"},
 		plugin = { "pretty", "json:target/cucumber-reports/Cucumber.json",
 				"junit:target/cucumber-reports/Cucumber.xml",
